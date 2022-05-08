a9bc06855f1f7ce5412a91bcec87c365140ee2d7
Arian_Berisha <arian.berisha@frakton.com>
2022-05-08 16:23:16 +0200

    add extra features

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
 import './App.css';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const config = {
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App bg-black h-4 w-11">
+      <ToastContainer {...config} />
+      <QueryClientProvider client={queryClient}>
 
+      </QueryClientProvider>
     </div>
   );
 }
